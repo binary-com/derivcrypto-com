bcab7ad1cb1525bbcb18fa17364e31aac11e6043
cakasuma <amammustofa@gmail.com>
2020-09-15 17:16:05 +0800

    build: add device, feat: upload and download script for translation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Nav } from 'components/elements/nav'
-import { WithIntl } from 'components/localization'
+import { localize, Localize, WithIntl } from 'components/localization'
 
 const Home = () => {
     return (
         <Nav>
-            <p>Hello world!</p>
+            <p>{localize('Deriv with Crypto')}</p>
+            <p>
+                <Localize translate_text="Trade forex, commodities, cryptocurrencies, synthetic and stock indices - commission-free and directly from your crypto wallets." />
+            </p>
         </Nav>
     )
 }
---
+++ src/themes/device.js
@@ -0,0 +1,13 @@
+export const size = {
+    mobile: 576,
+    tablet: 768,
+    laptop: 992,
+    desktop: 1200,
+}
+
+export const device = {
+    mobile: `(min-width: ${size.mobile}px)`,
+    tablet: `(min-width: ${size.tablet}px)`,
+    laptop: `(min-width: ${size.laptop}px)`,
+    desktop: `(min-width: ${size.desktop}px)`,
+}
