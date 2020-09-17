d5bef36cd5de678a85197432e6d98722e30758f3
cakasuma <amammustofa@gmail.com>
2020-09-17 16:23:19 +0800

    feat: added file system for graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,6 +30,13 @@ module.exports = {
             },
         },
         {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                name: 'images',
+                path: `${__dirname}/src/images/png`,
+            },
+        },
+        {
             resolve: 'gatsby-plugin-robots-txt',
             options: {
                 policy: [
