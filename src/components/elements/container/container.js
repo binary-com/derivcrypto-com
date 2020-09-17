8092c02f5756a2910bdd575d4d17d4bcdbcac602
cakasuma <amammustofa@gmail.com>
2020-09-17 14:17:10 +0800

    build: add container components

---
+++ src/components/elements/container/container.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import { device } from 'themes'
+
+export const Container = styled.div`
+    width: 100%;
+    padding-right: 16px;
+    padding-left: 16px;
+    margin-right: auto;
+    margin-left: auto;
+
+    @media ${device.mobile} {
+        max-width: 540px;
+    }
+    @media ${device.tablet} {
+        max-width: 720px;
+    }
+    @media ${device.laptop} {
+        max-width: 960px;
+    }
+    @media ${device.desktop} {
+        max-width: 1140px;
+    }
+`
+
+export const SectionContainer = styled.div`
+    margin: 40px 0;
+`
---
+++ src/components/elements/container/flex.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Flex = styled.div`
+    display: flex;
+`
+
+export const CenterFlex = styled(Flex)`
+    align-items: center;
+    justify-content: center;
+`
+
+export const VerticalFlex = styled(Flex)`
+    flex-direction: column;
+`
