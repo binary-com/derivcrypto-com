31db9ba6305a095864b67680696dc9046043785b
cakasuma <amammustofa@gmail.com>
2020-09-18 15:29:27 +0800

    build: add styled system to components

--- src/components/elements/container/flex.js
+++ src/components/elements/container/flex.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
+import { space, layout, flexbox } from 'styled-system'
 
 export const Flex = styled.div`
     display: flex;
+    ${space}
+    ${layout}
+    ${flexbox}
 `
 
 export const CenterFlex = styled(Flex)`
--- src/components/elements/input/input.js
+++ src/components/elements/input/input.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { InputStyle, ErrorText } from './input-style'
 import { VFlex } from 'components/elements'
 
-export const Input = ({ error, ...props }) => {
+export const Input = ({ error, className, ...props }) => {
     return (
-        <VFlex>
+        <VFlex className={className}>
             <InputStyle {...props} />
             {error && <ErrorText>{error}</ErrorText>}
         </VFlex>
