051357f3bb2062f3f321dfeb95fc23278b762e84
suthesan <suthesankanagasabai@gmail.com>
2020-09-18 14:07:56 +0800

    apply styles for hero

--- src/pages/home/_hero.js
+++ src/pages/home/_hero.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { StyledButton, StyledSectionContainer } from './_home-style'
+import { StyledButton, StyledSectionContainer, StyledInput, StyledText } from './_home-style'
 import { Container, Flex } from 'components/elements/container'
-import { Input } from 'components/elements/input'
+//import { Input } from 'components/elements/input'
 import { Text } from 'components/elements'
 import { Image } from 'components/elements/image'
 import { localize } from 'components/localization'
@@ -25,13 +25,13 @@ export const Hero = () => {
                 <Text as="h1" size="3xl">
                     {localize('Deriv with Crypto')}
                 </Text>
-                <Text as="p" size="l">
+                <StyledText as="p" size="l">
                     {localize(
                         'Trade forex, commodities, cryptocurrencies, synthetic and stock indices - commission-free and directly from your crypto wallets.',
                     )}
-                </Text>
+                </StyledText>
                 <Image data={data.hero} alt="platform devices" width="100%" height="181px" />
-                <Input placeholder={localize('Your email')} />
+                <StyledInput placeholder={localize('Your email')} />
                 <StyledButton primary>{localize('Get started')}</StyledButton>
                 <Flex>
                     <Text as="p" size="s">
--- src/pages/home/_home-style.js
+++ src/pages/home/_home-style.js
@@ -1,14 +1,22 @@
 import styled from 'styled-components'
-import { Button } from 'components/elements/button'
-import { SectionContainer } from 'components/elements/container'
+import { Input, Text, SectionContainer, Button } from 'components/elements'
 
 export const StyledButton = styled(Button)`
     display: flex;
-    margin: 0 auto;
+    width: 100%;
+    margin: 8px auto 13px;
     border: unset;
     justify-content: center;
 `
 
 export const StyledSectionContainer = styled(SectionContainer)`
     background-color: black;
+`
+
+export const StyledInput = styled(Input)`
+    margin-top: 40px;
+`
+
+export const StyledText = styled(Text)`
+    margin: 16px auto 40px;
 `
