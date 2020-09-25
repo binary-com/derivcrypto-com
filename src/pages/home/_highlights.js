b0be49d36a5828a533719a542d08c7cc12423832
suthesan <suthesankanagasabai@gmail.com>
2020-09-25 17:36:01 +0800

    completed responsive design & added styled theme to image component

--- src/pages/home/_highlights.js
+++ src/pages/home/_highlights.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
-import { PartitionContainer } from './_home-style'
+import { PartitionContainer, TextWrapper, ContentWrapper, HighLightsImage } from './_home-style'
 import { localize } from 'components/localization'
-import { SectionContainer, Container, Text, Image } from 'components/elements'
+import { SectionContainer, Text } from 'components/elements'
 
 const query = graphql`
     query {
@@ -23,43 +23,81 @@ export const Highlights = () => {
     return (
         <SectionContainer>
             <PartitionContainer no_padding_top>
-                <Container>
-                    <Text mb="m" as="h3" textAlign="center">
-                        {localize('Anywhere, anytime')}
-                    </Text>
-                    <Text m="auto" textAlign="justify">
-                        {localize(
-                            'Get on-board hassle-free, trade, and transfer funds to and from your trading account in the fewest taps on your mobile, or clicks on your computer.',
-                        )}
-                    </Text>
-                    <Image data={data.anytime} alt="platform devices" width="100%" height="100%" />
-                </Container>
+                <ContentWrapper>
+                    <TextWrapper>
+                        <Text
+                            fontSize={{ _: 'xl', xxl: '4xl' }}
+                            mb="m"
+                            as="h3"
+                            textAlign={{ _: 'center', xxl: 'left' }}
+                        >
+                            {localize('Anywhere, anytime')}
+                        </Text>
+                        <Text fontSize={{ _: 'm', xxl: 'xl' }} m="auto" textAlign="justify">
+                            {localize(
+                                'Get on-board hassle-free, trade, and transfer funds to and from your trading account in the fewest taps on your mobile, or clicks on your computer.',
+                            )}
+                        </Text>
+                    </TextWrapper>
+                    <HighLightsImage
+                        data={data.anytime}
+                        alt="platform devices"
+                        width={{ _: '300px', xxl: '588px' }}
+                        height={{ _: '374px', xxl: '690px' }}
+                    />
+                </ContentWrapper>
             </PartitionContainer>
             <PartitionContainer greybackground>
-                <Container>
-                    <Text mb="m" as="h3" textAlign="center">
-                        {localize('Crypto wallets included')}
-                    </Text>
-                    <Text m="auto" textAlign="justify">
-                        {localize(
-                            'Get on-board hassle-free, trade, and transfer funds to and from your trading account in the fewest taps on your mobile, or clicks on your computer.',
-                        )}
-                    </Text>
-                    <Image data={data.crypto} alt="platform devices" width="100%" height="100%" />
-                </Container>
+                <ContentWrapper>
+                    <TextWrapper>
+                        <Text
+                            fontSize={{ _: 'xl', xxl: '4xl' }}
+                            mb="m"
+                            as="h3"
+                            textAlign={{ _: 'center', xxl: 'left' }}
+                        >
+                            {localize('Crypto wallets included')}
+                        </Text>
+                        <Text fontSize={{ _: 'm', xxl: 'xl' }} m="auto" textAlign="justify">
+                            {localize(
+                                'Get on-board hassle-free, trade, and transfer funds to and from your trading account in the fewest taps on your mobile, or clicks on your computer.',
+                            )}
+                        </Text>
+                    </TextWrapper>
+                    <HighLightsImage
+                        margin="auto"
+                        data={data.crypto}
+                        alt="platform devices"
+                        width={{ _: '292px', xxl: '528px' }}
+                        height={{ _: '288px', xxl: '528px' }}
+                    />
+                </ContentWrapper>
             </PartitionContainer>
             <PartitionContainer>
-                <Container>
-                    <Text mb="m" as="h3" textAlign="center">
-                        {localize('No minimum deposit')}
-                    </Text>
-                    <Text m="auto" textAlign="justify">
-                        {localize(
-                            'With no minimum deposit and low stake amounts, anyone can start trading from anywhere.',
-                        )}
-                    </Text>
-                    <Image data={data.deposit} alt="platform devices" width="100%" height="100%" />
-                </Container>
+                <ContentWrapper>
+                    <TextWrapper>
+                        <Text
+                            fontSize={{ _: 'xl', xxl: '4xl' }}
+                            mb="m"
+                            as="h3"
+                            textAlign={{ _: 'center', xxl: 'left' }}
+                        >
+                            {localize('No minimum deposit')}
+                        </Text>
+                        <Text fontSize={{ _: 'm', xxl: 'xl' }} m="auto" textAlign="justify">
+                            {localize(
+                                'With no minimum deposit and low stake amounts, anyone can start trading from anywhere.',
+                            )}
+                        </Text>
+                    </TextWrapper>
+                    <HighLightsImage
+                        margin="auto"
+                        data={data.deposit}
+                        alt="platform devices"
+                        width={{ _: '276px', xxl: '528px' }}
+                        height={{ _: '164px', xxl: '314px' }}
+                    />
+                </ContentWrapper>
             </PartitionContainer>
         </SectionContainer>
     )
