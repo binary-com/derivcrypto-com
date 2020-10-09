2863b95c94df51f773b3d273adc6638b64d3d4da
AbdallaFawzy <abdullah.f98m@gmail.com>
2020-10-09 13:16:06 +0800

    add signup through facebook and google

--- src/common/login.js
+++ src/common/login.js
@@ -31,11 +31,10 @@ const Login = (() => {
         const affiliate_token_link = affiliate_tracking
             ? `&affiliate_token=${affiliate_tracking}`
             : ''
-        const deriv_app_app_id = 16929
 
         return server_url && /qa/.test(server_url)
             ? `https://${server_url}/oauth2/authorize?app_id=${getAppId()}&l=${language}${marketing_queries}&brand=${app_name.toLowerCase()}${affiliate_token_link}${utm_source_link}${utm_medium_link}${utm_campaign_link}`
-            : `https://oauth.deriv.com/oauth2/authorize?app_id=${deriv_app_app_id}&l=${language}${marketing_queries}&brand=${app_name.toLowerCase()}${affiliate_token_link}${utm_source_link}${utm_medium_link}${utm_campaign_link}`
+            : `https://oauth.deriv.com/oauth2/authorize?app_id=${getAppId()}&l=${language}${marketing_queries}&brand=${app_name.toLowerCase()}${affiliate_token_link}${utm_source_link}${utm_medium_link}${utm_campaign_link}`
     }
 
     const initOneAll = provider => {
